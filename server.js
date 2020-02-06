82f3dedc0dc987447c1ee8cf8ac075218dca6a3f
Tyler Snyder <tyler@tylers-mbp.uguest.utah.edu>
2020-02-05 19:12:57 -0700

    set up server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
