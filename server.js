721fdede0d3a4250bb3139b4b78c75e3fbb31669
Tyler Snyder <tyler@tylers-mbp.uguest.utah.edu>
2020-02-05 19:26:12 -0700

    added routes

--- server.js
+++ server.js
@@ -10,6 +10,19 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
